41f866e34eaa3d78a51a6102211057f330d25a47
Geoffrey Bachelet <grosfrais@gmail.com>
2014-05-02 11:06:48 +0200

    source phpenv.sh

--- docker.sh
+++ docker.sh
@@ -11,4 +11,5 @@ docker -d -H 0.0.0.0:4243 -H unix:///var/run/docker.sock 2>> /dev/null >> /dev/n
 sleep 2
 
 # $init is set by sekexe
+source /etc/profile.d/phpenv.sh
 cd $(dirname $init)/.. && php bin/phpunit -c phpunit.xml.dist
